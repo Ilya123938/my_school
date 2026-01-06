a085ae4b7fae7927391d0c6d16df589bb55694d5
Ilyasse Lehhit <ilehhit@gmail.com>
2026-01-06 17:05:07 +0100

    Fix CSRF and static/media settings for Railway

--- config/settings.py
+++ config/settings.py
@@ -122,13 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT =os.path.join(BASE_DIR,'static')
+STATIC_URL = '/static/'
+STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'account/static')]
 
 # MEIDA FILES
 MEDIA_URL ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'mediafiles')
 
 
 AUTH_USER_MODEL = 'account.Account'
